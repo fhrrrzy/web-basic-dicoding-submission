2af9e380a37d91db35950a36dc849eb4e5495018
fhrrrzy <fahrurrozy4214@gmail.com>
2023-03-03 02:56:10 +0700

    fix under development alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,13 @@ document.onpointermove = function(event){
 const button = document.getElementsByTagName("button")
 const noContent = document.getElementsByClassName("no-content")[0]
 
-noContent.addEventListener("click", function(){
-    noContent.style.display = "none"
-})
 
 for(let i = 0; i < button.length ; i++){
     button[i].addEventListener("click", function(){
         noContent.style.display = "block"
     })
 }
+
+noContent.addEventListener("click", function(){
+    noContent.style.display = "none"
+})
